c8ace236eb56fb2a02535552621ffa514af5b15c
idionkim <ksundong@gmail.com>
2019-08-10 08:54:41 +0900

    replace Constructor with lombok's AllArgsConstructor Annotation.

--- src/main/java/org/zerock/sample/SampleHotel.java
+++ src/main/java/org/zerock/sample/SampleHotel.java
@@ -1,5 +1,6 @@
 package org.zerock.sample;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.stereotype.Component;
@@ -7,14 +8,9 @@ import org.springframework.stereotype.Component;
 @Component
 @ToString
 @Getter
+@AllArgsConstructor
 public class SampleHotel {
 	
 	// 묵시적 생성자 주입
 	private Chef chef;
-	
-	public SampleHotel(Chef chef) {
-		
-		this.chef = chef;
-		
-	}
 }
